dc877ff36c0ba19fcc5cd6a2ba9fe2be895a7591
ishofstede <101320849+ishofstede@users.noreply.github.com>
2025-01-13 22:09:59 +0100

    updated module call

--- notebooks/python/__init__.py
+++ notebooks/python/__init__.py
@@ -3,4 +3,4 @@ This package contains the source code for the project.
 """
 
 from .data_processing import DataProcessing
-from data_generator import imageLoader
+from .data_generator import DataGenerator
