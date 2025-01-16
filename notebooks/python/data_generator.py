e5504fd40eac5de0a11983dcdec5030b9a22b76d
ishofstede <101320849+ishofstede@users.noreply.github.com>
2025-01-16 11:53:27 +0100

    updated data generator to cast float 64 into 34

--- notebooks/python/data_generator.py
+++ notebooks/python/data_generator.py
@@ -7,10 +7,8 @@ No image processing operations are performed here, just load data from local dir
 in batches. 
 
 """
-
-#from tifffile import imsave, imread
-import os
 import numpy as np
+import os 
 
 class DataGenerator:
     @staticmethod
@@ -18,9 +16,9 @@ class DataGenerator:
         images = []
         for i, image_name in enumerate(img_list):    
             if image_name.split('.')[1] == 'npy':
-                image = np.load(img_dir + image_name)
+                image = np.load(os.path.join(img_dir, image_name))
                 images.append(image)
-        images = np.array(images)
+        images = np.array(images, dtype=np.float32)  # Ensure data type is float32
         return images
 
     @staticmethod
@@ -39,7 +37,12 @@ class DataGenerator:
                 X = DataGenerator.load_img(img_dir, img_list[batch_start:limit])
                 Y = DataGenerator.load_img(mask_dir, mask_list[batch_start:limit])
 
-                yield (X, Y)  # a tuple with two numpy arrays with batch_size samples     
+                # Cast to float32 to ensure consistency
+                X = X.astype(np.float32)
+                Y = Y.astype(np.float32)
+
+                yield (X, Y)  # a tuple with two numpy arrays with batch_size samples
 
                 batch_start += batch_size   
                 batch_end += batch_size
+
